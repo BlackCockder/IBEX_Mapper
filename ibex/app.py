ccc91a8af526322bd68fe083c908c387cd303e18
BlackCockder <makobi99@gmail.com>
2025-07-01 16:33:16 +0200

    Introduced core calculations to calculator, defined first method that is callable from imports on IBEXMapper

--- ibex/app.py
+++ ibex/app.py
@@ -1,9 +1,15 @@
 from calculator import Calculator
 from configurator import Configurator
 from projection import Projection
+import numpy as np
+
 
 class IBEXMapper:
     def __init__(self, projection: Projection, calculator: Calculator, configurator: Configurator) -> None:
         self.projection = projection
         self.calculator = calculator
         self.configurator = configurator
+
+    def generateMapFromLink(self, link: str, accuracy: int) -> None:
+        imported_data = np.loadtxt(link, comments='#')
+        self.projection.projection(self.calculator.calculateMainFunctionFromData(imported_data, accuracy), accuracy)
