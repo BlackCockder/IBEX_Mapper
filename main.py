e79fbe4d2cd6e3b16144753553b12174aa52abd1
BlackCockder <makobi99@gmail.com>
2025-07-01 10:35:34 +0200

    Filter class added

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ class Main:
     def __init__(self, name, parameters):
         self.name = name
         self.parameters = parameters
+
+
+class Filterer:
+    def __init__(self, name, parameters):
+        self.name = name
+        self.parameters = parameters
