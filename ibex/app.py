3d981e3377c1d3cc90fdaf9b75173502ccff0cf8
BlackCockder <makobi99@gmail.com>
2025-07-01 15:03:27 +0200

    Merge attempt 2

--- ibex/app.py
+++ ibex/app.py
@@ -1,9 +1,9 @@
 from calculator import Calculator
 from configurator import Configurator
-
+from projection import Projection
 
 class IBEXMapper:
-    def __init__(self, projection, calculator: Calculator, configurator: Configurator) -> None:
+    def __init__(self, projection: Projection, calculator: Calculator, configurator: Configurator) -> None:
         self.projection = projection
         self.calculator = calculator
         self.configurator = configurator
