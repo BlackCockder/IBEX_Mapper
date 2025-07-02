Calculation logic rework, bugfix in projection, CLI modifications

ibex/calculator.py
@@ -7,6 +7,15 @@ class Calculator:
         pass
 
     def calculateMainFunctionFromData(self, data, dpi):
-        latitude, longitude = np.meshgrid(np.linspace(0, 2*np.pi, dpi), np.linspace(0, np.pi, dpi))
-        result_array = spherical_harmonics(data[:, 0], data[:, 1], latitude, longitude)
-        return np.sum(data[:, 2] * result_array).real
+        colatitude, longitude = np.meshgrid(np.linspace(0, np.pi, dpi), np.linspace(0, 2*np.pi, dpi))
+        heatmapData = np.zeros((dpi, dpi), dtype=np.complex128)
+        i = 0
+        for row in data:
+            l = row[0].astype(int)
+            m = row[1].astype(int)
+            coeff = row[2]
+            Y_lm = spherical_harmonics(m, l, longitude, colatitude)
+            heatmapData += coeff * Y_lm
+            i += 1
+            print(f"Calculating... {round((i/data.shape[0]) * 100, 2)}%")
+        return heatmapData.real

ibex/projection.py
@@ -16,7 +16,7 @@ class Projection:
         lon_r , lat_r = np.deg2rad(lon), np.deg2rad(lat) # in radians
 
         fig = plt.figure(figsize=(8, 6))
-        ax = fig.add_subplot(z, projection="mollweide")
+        ax = fig.add_subplot(111, projection="mollweide")
 
         pcm = ax.pcolormesh(lon_r, lat_r, z, cmap="viridis", shading="auto")
         cbar = fig.colorbar(pcm, ax=ax, orientation="horizontal", pad=0.05)