b42ba6295984885c58601c54421f33de43d4587f
BlackCockder <makobi99@gmail.com>
2025-07-07 13:04:05 +0200

    Added docstrings and first comments

--- IBEXMapper/handler.py
+++ IBEXMapper/handler.py
@@ -8,18 +8,46 @@ class Handler:
         self.calculator = calculator
 
     def processUserDataset(self, dpi: int, target_max_l: int, data: np.ndarray) -> np.ndarray:
+        """
+        Main function that generates data for heatmap before configuration is applied.
+
+        :param dpi:
+        Resolution of the map. Ranges from 0 to infinity but generally speaking the intended accuracy is around 720.
+
+        :param target_max_l:
+        Caching parameter. Ranges from 0 to infinity. Determines how deep will calculator calculate the spherical
+        harmonics for. Intended value is one that user sets as default in the config.
+
+        :param data:
+        Matrix of (N, 3) size. Generally used for accessing 3rd column where the coefficients are located and for
+        cutting the cached spherical harmonics data to match the row length for vectorized matrix multiplication in
+        calculator.
+
+        :returns:
+        Returns (dpi, dpi) size matrix of data for heatmap.
+        """
+
+        # Generating file paths for potential caching
         cache_dir = Path(__file__).resolve().parent / "cache"
         cache_dir.mkdir(exist_ok=True)
-
         file_name = f"DPI{dpi}L{target_max_l}.npy"
         file_path = cache_dir / file_name
 
         if self.checkForCachedSphericalHarmonics(file_path):
-            return self.calculator.calculateMainMatrixFromData(data, self.loadSphericalHarmonicsFromCache(file_path)[:data.shape[0]], dpi)
+            # We can cut it directly here because in app.py there is data sanitization that checks whether the inputted
+            # file and inputted max_l are properly defined (meaning always max_l >= count_of_rows).
+            cached_spherical_harmonics = self.loadSphericalHarmonicsFromCache(file_path)[:data.shape[0]]
+
+            return self.calculator.calculateMainMatrixFromData(data, cached_spherical_harmonics, dpi)
         else:
             spherical_harmonics_matrices = self.calculator.calculateSphericalHarmonicsDataForSetDPI(dpi, target_max_l)
+
             self.cacheSphericalHarmonics(file_path, spherical_harmonics_matrices)
-            return self.calculator.calculateMainMatrixFromData(data, self.loadSphericalHarmonicsFromCache(file_path)[:data.shape[0]], dpi)
+
+            # Same assumption here as in line 39.
+            cut_spherical_harmonics = spherical_harmonics_matrices[:data.shape[0]]
+
+            return self.calculator.calculateMainMatrixFromData(data, cut_spherical_harmonics, dpi)
 
     def checkForCachedSphericalHarmonics(self, file_path) -> bool:
         return file_path.exists()
