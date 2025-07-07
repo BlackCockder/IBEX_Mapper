86fdf51d7a714f91897e2de0d588a14384966844
BlackCockder <makobi99@gmail.com>
2025-07-07 12:14:39 +0200

    Split built_rotation into two separate functions

--- IBEXMapper/configurator.py
+++ IBEXMapper/configurator.py
@@ -1,31 +1,34 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from calculator import Calculator
+
 
 class Configurator:
-    def __init__(self):
-        pass
+    def __init__(self, calculator: Calculator):
+        self.calculator = calculator
 
-    def sph2cart(self, lon: float, lat: float) -> np.ndarray:
+    def convertSphericalToCartesianForPoints(self, lon: float, lat: float) -> np.ndarray:
+        lon = np.deg2rad(lon)
+        lat = np.deg2rad(lat)
         x: float = np.cos(lat) * np.cos(lon)
         y: float = np.cos(lon) * np.sin(lat)
         z: float = np.sin(lat)
         return np.array([x, y, z])
 
-    def build_rotation(self, v1_lon: float, v1_lat: float, v2_lon: float, v2_lat: float) -> float:
-        x_axis = np.array([1., 0., 0.])
+    def buildCenteringRotation(self, new_central_vector: np.ndarray) -> np.ndarray:
+        current_central_vector = np.array([1., 0., 0.])
+        new_central_vector = new_central_vector / np.linalg.norm(new_central_vector)
 
-        # conversion from degrees to radians
-        v1_lon_rad, v1_lat_rad = np.deg2rad(v1_lon), np.deg2rad(v1_lat)
-        v2_lon_rad, v2_lat_rad = np.deg2rad(v2_lon), np.deg2rad(v2_lat)
+        central_vector_in_cartesian: np.ndarray = self.convertSphericalToCartesianForPoints(new_central_vector[0], new_central_vector[1])
 
-        # converting to cartesian coordinates
-        v1: np.ndarray = self.sph2cart(v1_lon_rad, v1_lat_rad)
-        v2: np.ndarray = self.sph2cart(v2_lon_rad, v2_lat_rad)
+        crossproduct_vector: np.ndarray = np.cross(current_central_vector, central_vector_in_cartesian)
 
-        R1, _ = R.align_vectors([x_axis], [v1])
+        alpha_in_degrees = np.clip(np.dot(current_central_vector, central_vector_in_cartesian), -1.0, 1.0)
+        alpha_in_radians = np.arccos(alpha_in_degrees)
 
-        v2p = R1.apply(v2)
-        beta = np.arctan2(v2p[1], v2p[2])
-        R2 = R.from_euler("x", beta)
+        return R.from_rotvec(alpha_in_radians * crossproduct_vector).as_matrix()
 
-        return R2 * R1 # whole rotation matrix
+    def buildAligningRotation(self, vector_to_align: np.ndarray) -> np.ndarray:
+        vector_to_align_in_cartesian = self.convertSphericalToCartesianForPoints(vector_to_align[0], vector_to_align[1])
+        beta = np.arctan2(vector_to_align_in_cartesian[1], vector_to_align_in_cartesian[2])
+        return R.from_euler("x", beta).as_matrix()
